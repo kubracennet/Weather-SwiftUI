f22efe154ea13b1339cc4552ffe755df7af53e6e
kubracennet <kubracennetyavasoglu@gmail.com>
2023-03-25 20:26:21 +0300

    Initial Commit

---
+++ Weather-SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather-SwiftUI
+//
+//  Created by Kübra Cennet Yavaşoğlu on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
